cd5dce9095214a49cdfe63f40849c70c54415a1e
신진범 <shjb07015@gmail.com>
2025-03-27 17:03:04 +0900

    Initial commit

---
+++ src/main/java/com/example/RealConnect/RealConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.RealConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RealConnect/RealConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RealConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
