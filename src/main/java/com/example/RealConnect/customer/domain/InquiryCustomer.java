e5a72a3fb1e2f52d7b82daa077eda551aa90bf21
신진범 <shjb07015@gmail.com>
2025-04-30 17:06:55 +0900

    feat. 엔티티 수정

--- src/main/java/com/example/RealConnect/customer/domain/InquiryCustomer.java
+++ src/main/java/com/example/RealConnect/customer/domain/InquiryCustomer.java
@@ -23,29 +23,43 @@ public class InquiryCustomer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * 문의 담당 중개사
+     */
     @ManyToOne(fetch = FetchType.LAZY)
     private User agent;
 
+    /**
+     * 문의고객 이름
+     */
     private String name;
 
+    /**
+     * 문의고객 전화번호
+     */
     private String phone;
 
  ///////////////////////요구사항//////////////////////////////////
-    private String apartmentName;
-    private int Dong;
-    private int Ho;
-    private double area;
-    private Direction direction;
+
+    /**
+     * 문의유형
+     *  매매| 전세 | 월세
+     */
+    private String inquiryType;
+    private String apartmentName; //단지
+    private double area; //면적
     private Long salePrice; //매매가
     private Long jeonsePrice; //전세가
     private Long deposit; //보증금
     private Long monthPrice; //월세
 ///////////////////////////////////////////////////////////////////////
 
-
+    /**
+     * 문의내용
+     */
     private String memo;
 
-    /*
+    /**
         즐겨찾기
      */
     private boolean favorite;
