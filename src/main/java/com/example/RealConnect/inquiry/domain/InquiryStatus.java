0ea5e85501c18e67d630c97db685025fc29da752
Kim-Mark <qsik1218000@gmail.com>
2025-05-03 11:19:09 +0900

    test: test위한 코드 삭제

--- src/main/java/com/example/RealConnect/inquiry/domain/InquiryStatus.java
+++ src/main/java/com/example/RealConnect/inquiry/domain/InquiryStatus.java
@@ -1,7 +1,7 @@
 package com.example.RealConnect.inquiry.domain;
 
 public enum InquiryStatus {
-    // 진행 상태 - 진행 중 / 진행 완료 /전체
+    // 진행 상태 - 진행 중 / 진행 완료
         IN_PROGRESS,
         COMPLETED
 
