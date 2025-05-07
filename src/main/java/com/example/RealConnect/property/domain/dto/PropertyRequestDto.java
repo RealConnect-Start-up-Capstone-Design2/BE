fdfe1c147c7bebceffda4976ad8cb17840e082e1
Choi-89 <wlghks3418@naver.com>
2025-05-08 01:00:15 +0900

    feat: 모든 DTO에 not null 추가

--- src/main/java/com/example/RealConnect/property/domain/dto/PropertyRequestDto.java
+++ src/main/java/com/example/RealConnect/property/domain/dto/PropertyRequestDto.java
@@ -1,6 +1,8 @@
 package com.example.RealConnect.property.domain.dto;
 
 import com.example.RealConnect.property.domain.PropertyStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,21 +11,29 @@ import lombok.Setter;
 @Setter
 public class PropertyRequestDto {
     // 아파트 id
+    @NotNull(message = "아파트 아이디를 입력하세요.")
     private Long apartmentId;
 
     // 중개사 id
+    @NotNull(message = "중개사 아이디를 입력하세요.")
     private Long agentId;
 
     // 소유자
+    @NotBlank(message = "소유자 이름을 입력하세요.")
     private String ownerName;
+    @NotBlank(message = "소유자 전화번호를 입력하세요.")
     private String ownerPhone;
 
     // 임차인
+    @NotBlank(message = "임차인 이름을 입력하세요.")
     private String tenantName;
+    @NotBlank(message = "임차인 전화번호를 입력하세요.")
     private String tenantPhone;
 
     // 거래
+    @NotNull(message = "거래 상태를 입력하세요.")
     private boolean isSale;
+    @NotNull(message = "거래 가격을 입력하세요.")
     private Long salePrice;
 
     private boolean isJeonse;
--- src/main/java/com/example/RealConnect/property/domain/dto/PropertyStatusDto.java
+++ src/main/java/com/example/RealConnect/property/domain/dto/PropertyStatusDto.java
@@ -1,10 +1,13 @@
 package com.example.RealConnect.property.domain.dto;
 
 import com.example.RealConnect.property.domain.PropertyStatus;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class PropertyStatusDto {
+    @NotNull(message = "매물 아이디를 작성하세요.")
     private Long id;
+    @NotNull(message = "상태를 작성하세요.")
     private PropertyStatus Status;
 }
