2f7743f49c09fd50f1c1d72f7881d430a2bc483c
Choi-89 <wlghks3418@naver.com>
2025-05-08 23:38:26 +0900

    remove : 불필요한 패키지 제거

--- src/main/java/com/example/RealConnect/properties/repository/PropertyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.RealConnect.properties.repository;
-
-import com.example.RealConnect.properties.domain.Property;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PropertyRepository extends JpaRepository<Property, Long> {
-
-}
