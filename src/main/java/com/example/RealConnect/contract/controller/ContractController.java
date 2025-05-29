9292f070df1f370ccde49bc2576ff6ae602b1f40
신진범 <140528690+bumsoft@users.noreply.github.com>
2025-05-29 14:35:56 +0900

    feat: 예외처리 및 반환 - 예외처리 - 생성 / 수정 성공 시 해당 dto 반환

--- src/main/java/com/example/RealConnect/contract/controller/ContractController.java
+++ src/main/java/com/example/RealConnect/contract/controller/ContractController.java
@@ -1,5 +1,7 @@
 package com.example.RealConnect.contract.controller;
 
+import com.example.RealConnect.contract.Exception.ContractNotFoundException;
+import com.example.RealConnect.contract.Exception.ContractNotMatchException;
 import com.example.RealConnect.contract.Exception.InvalidContractTypeException;
 import com.example.RealConnect.contract.domain.ContractPostRequestDto;
 import com.example.RealConnect.contract.domain.ContractResponseDto;
@@ -23,31 +25,9 @@ public class ContractController {
 
     // 계약등록
     @PostMapping("/api/contract")
-    public ResponseEntity<Void> registerDirectContract(@RequestBody ContractPostRequestDto dto, Principal principal) {
-        contractService.registerDirectContract(dto, principal.getName());  // 직접 계약 등록
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
-
-
-    // 1. 매물 관리에서 계약 등록 - ContractPostResponseDto 받아서 처리
-    // requestbody - json으로 전달되는 데이터를 dto로 바인딩
-    @PostMapping("/api/contract/registerFromProperty")
-    public ResponseEntity<Void> registerContractFromProperty(
-            @RequestBody ContractPostRequestDto dto,
-            Principal principal)
-    {
-        contractService.registerContractFromProperty(dto, principal.getName());
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
-
-    // 2. 문의 관리에서 계약 등록
-    @PostMapping("/api/contract/registerFromInquiry")
-    public ResponseEntity<Void> registerContractFromInquiry(
-            @RequestBody ContractPostRequestDto dto,
-            Principal principal)
-    {
-        contractService.registerContractFromInquiry(dto, principal.getName());
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    public ResponseEntity<?> registerDirectContract(@RequestBody ContractPostRequestDto dto, Principal principal) {
+        ContractResponseDto responseDto = contractService.createContract(dto, principal.getName());
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
     }
 
     // 계약 조회의 종류 - 전체 / 즐겨찾기 / 계약 상태 / 통합 검색
@@ -66,19 +46,19 @@ public class ContractController {
 
     // 계약 수정
     @PutMapping("/api/contract/update/{contractId}")
-    public ResponseEntity<Void> updateContract(
+    public ResponseEntity<?> updateContract(
             @PathVariable Long contractId,
             @RequestBody ContractPostRequestDto dto,
             Principal principal // 현재 로그인된 사용자
     ) {
-        contractService.updateContract(contractId, dto, principal.getName());  // 계약 존재 여부 및 권한 검증 후 계약 수정
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();  // 204 No Content 반환
+        ContractResponseDto responseDto = contractService.updateContract(contractId, dto, principal.getName());// 계약 존재 여부 및 권한 검증 후 계약 수정
+        return ResponseEntity.ok(responseDto);
     }
 
     // 계약삭제
     @DeleteMapping("/api/contract/delete/{contractId}")
-    public ResponseEntity<Void> deleteContract(@PathVariable Long contractId) {
-        contractService.deleteContract(contractId);  // 계약 삭제 처리
+    public ResponseEntity<Void> deleteContract(@PathVariable Long contractId, Principal principal) {
+        contractService.deleteContract(contractId, principal.getName());  // 계약 삭제 처리
         return ResponseEntity.noContent().build();  // 성공적으로 삭제되면 204 No Content 반환
     }
 
@@ -87,4 +67,27 @@ public class ContractController {
     public ResponseEntity<String> handleInvalidContractTypeException(InvalidContractTypeException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
+
+    /**
+     * 존재하지 않는 계약에 대한 요청
+     * @param e
+     * @return 404
+     */
+    @ExceptionHandler({ContractNotFoundException.class})
+    public ResponseEntity<String> handleContractNotFoundException(ContractNotFoundException e)
+    {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    /**
+     * 계약 권한 없는 경우
+     * @param e
+     * @return 403
+     */
+    @ExceptionHandler({ContractNotMatchException.class})
+    public ResponseEntity<String> handleContractNotMatchException(ContractNotMatchException e)
+    {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+    }
+
 }
